fc026a6584212f6d22c9d81c771aa15d59f1ebf1
YouMiNope <1795803836@qq.com>
2023-08-25 01:10:31 +0800

    imgui

--- RayTracingWeekend/RayTracingWeekend.h
+++ RayTracingWeekend/RayTracingWeekend.h
@@ -7,6 +7,10 @@
 #include <thread>
 #include <random>
 
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
 #include "utils.h"
 #include "color.h"
 #include "vec3.h"
